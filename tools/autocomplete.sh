86d9c01fdb1dd2b078c942a2ab1a571de385e7fa
Bruno Bernard <contact.brunobernard@gmail.com>
2025-05-06 13:29:28 +0400

    feat: add use

--- tools/autocomplete.sh
+++ tools/autocomplete.sh
@@ -4,7 +4,7 @@ _km_complete() {
   local cur prev opts
   COMPREPLY=()
   cur="${COMP_WORDS[COMP_CWORD]}"
-  opts="add switch backup-restore backup-list backup-prune delete rename-context list help"
+  opts="add use switch backup-restore backup-list backup-prune delete rename-context list help"
 
   if [[ ${COMP_CWORD} -eq 1 ]]; then
     COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
